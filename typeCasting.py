c78ec98b1589fd38d37982c5673230ee62771b77
dineshsonawane615 <dineshsonawane615@gmail.com>
2025-09-23 11:24:17 +0530

    -

---
+++ typeCasting.py
@@ -0,0 +1,24 @@
+#implicit casting
+num1=10
+num2=3.0
+div=num1/num2
+print(div)
+print("-----------------------------------------------------")
+#Explicit casting - User in python and explicit type casting
+num3=int(input("Enter num3: "))
+num4=int(input("Enter num4: ")) 
+print(f"sum of num is: {num3+num4}")
+
+x=int(4)            # x will be 4
+y=int(7)            # y will be 7
+z=int("89")         # z will be 89
+print(x, y, z)
+
+a=float(2)          # a will be 1.0
+b=float(2.5)        # b will be 2.5
+c=float("3")        # c will be 3.0       
+print(a, b, c)
+
+y=str(2)            #y will be '2'
+z=str(5.0)          #z will be '5.0'
+print(y, z)
